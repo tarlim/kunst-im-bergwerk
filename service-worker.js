d9076af0104e36036ce21165806373510836bb6d
tarlim <61198252+tarlim@users.noreply.github.com>
2025-06-30 16:53:41 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,37 @@
-const CACHE_NAME = "kunststollen-v1";
+const CACHE_NAME = "kunststollen-v2";
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/werke/werk1.html",
-  "/manifest.json"
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./werke/werk1.html"
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
   );
 });
